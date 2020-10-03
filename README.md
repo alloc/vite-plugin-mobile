83fb4ef83a2435da58c40f24f0e5cfa5026b6bab
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-10-03 11:19:32 -0400

    0.1.1

---
+++ README.md
@@ -0,0 +1,55 @@
+# vite-plugin-mobile
+
+- Serve different modules based on the [user agent].
+- Build separate bundles for mobile and desktop.
+- Easily share modules between them.
+
+[user agent]: https://en.wikipedia.org/wiki/User_agent
+
+```ts
+import mobile from 'vite-plugin-mobile'
+
+export default {
+  plugins: [
+    mobile(),
+  ]
+}
+```
+
+&nbsp;
+
+### Options
+
+- `mobileRoot: string`  
+  The directory containing mobile-only modules.  
+  Defaults to `"/src/mobile"`
+
+- `desktopRoot: string`  
+  The directory containing desktop-only modules.  
+  Defaults to `"/src/desktop"`
+
+- `tabletRoot: string`  
+  The directory containing tablet-only modules.  
+  Defaults to `this.mobileRoot`
+
+&nbsp;
+
+### Notes
+
+- "Common chunks" are *not* generated, so bundles *will* contain duplicate modules.
+- Assets in `public/` are shared between bundles.
+- Your `index.html` is rendered once per bundle.
+- The desktop bundle is used by `dist/index.html`.
+- The mobile bundle is used by `dist/index.mobile.html`.
+- When `tabletRoot` is unique, a separate tablet-only bundle is generated and
+  used by `dist/index.tablet.html`.
+
+&nbsp;
+
+### Compatibility
+
+The latest version of Vite does *not* support this plugin.
+
+You need to clone Vite locally and checkout this PR:
+
+- https://github.com/vitejs/vite/pull/878
---
+++ src/plugin.ts
@@ -0,0 +1,144 @@
+import type {
+  rollup as Rollup,
+  Plugin as RollupPlugin,
+  InputOptions,
+} from 'vite/node_modules/rollup'
+import { Plugin as VitePlugin, BuildConfig } from 'vite'
+import DeviceDetector from 'device-detector-js'
+import path from 'path'
+
+const isBuild = process.argv[2] == 'build'
+
+type Config = {
+  /**
+   * The directory containing phone-only modules.
+   * @default "/src/mobile"
+   */
+  mobileRoot?: string
+  /**
+   * The directory containing desktop-only modules.
+   * @default "/src/desktop"
+   */
+  desktopRoot?: string
+  /**
+   * The directory containing tablet-only modules.
+   * @default this.mobileRoot
+   */
+  tabletRoot?: string
+}
+
+const NODE_MODULES_DIR = path.sep + 'node_modules' + path.sep
+
+export default ({
+  mobileRoot = '/src/mobile',
+  desktopRoot = '/src/desktop',
+  tabletRoot = mobileRoot,
+}: Config = {}): VitePlugin => {
+  const roots = {
+    mobile: mobileRoot,
+    desktop: desktopRoot,
+    tablet: tabletRoot,
+  }
+  const uniqueRoots = uniq(roots)
+  const findRoot = (id: string) =>
+    uniqueRoots.find(root => id.startsWith(root + '/'))
+
+  if (isBuild) {
+    const createResolverPlugin = (
+      deviceType: 'mobile' | 'tablet',
+      config: Readonly<BuildConfig>
+    ): RollupPlugin => ({
+      name: 'vite-mobile:resolver',
+      async resolveId(id, importer) {
+        // Skip imports from node_modules.
+        if (importer && importer.indexOf(NODE_MODULES_DIR) < 0) {
+          // Resolve relative paths only.
+          if (!/^\.\.?\//.test(id)) return null
+
+          const resolved = await this.resolve(id, importer, {
+            skipSelf: true,
+          })
+          if (resolved) {
+            const moduleId = '/' + path.relative(config.root, resolved.id)
+            const moduleRoot = findRoot(moduleId)
+            if (moduleRoot)
+              return path.join(
+                config.root,
+                moduleId.replace(moduleRoot, roots[deviceType])
+              )
+          }
+        }
+        return null
+      },
+    })
+
+    const rollup = require('vite/node_modules/rollup').rollup as typeof Rollup
+    return {
+      configureBuild(config, builds) {
+        const { plugins = [] } = config.rollupInputOptions
+        config.rollupInputOptions.plugins = plugins
+
+        const createBuild = (
+          deviceType: 'mobile' | 'tablet',
+          inputOptions: Readonly<InputOptions>
+        ) => ({
+          // Emit "index.mobile.html" or "index.tablet.html"
+          id: 'index.' + deviceType,
+          // Wait until preceding bundles are finished.
+          get bundle() {
+            return rollup({
+              ...inputOptions,
+              plugins: [
+                createResolverPlugin(deviceType, config),
+                ...inputOptions.plugins!.filter(
+                  plugin => plugin.name !== 'vite-mobile:init'
+                ),
+              ],
+            })
+          },
+        })
+
+        // The mobile/tablet builds need the same options used by
+        // the desktop build, so we must wait for them.
+        plugins.push({
+          name: 'vite-mobile:init',
+          options(inputOptions) {
+            builds.push(createBuild('mobile', inputOptions))
+            if (uniqueRoots.length > 2)
+              builds.push(createBuild('tablet', inputOptions))
+
+            return null
+          },
+        })
+      },
+    }
+  }
+
+  return {
+    configureServer({ app }) {
+      const parser = new DeviceDetector({ skipBotDetection: true })
+      app.use(async (ctx, next) => {
+        if (!ctx.path.startsWith('/@modules/')) {
+          const moduleRoot = findRoot(ctx.path)
+          if (moduleRoot) {
+            const { device } = parser.parse(ctx.get('User-Agent'))
+            const deviceType =
+              (device &&
+                ((/tablet/.test(device.type) && 'tablet') ||
+                  (/phone/.test(device.type) && 'mobile'))) ||
+              'desktop'
+
+            if (moduleRoot !== roots[deviceType]) {
+              ctx.path = ctx.path.replace(moduleRoot, roots[deviceType])
+            }
+          }
+        }
+        await next()
+      })
+    },
+  }
+}
+
+function uniq<T>(values: { [key: string]: T }) {
+  return Array.from(new Set(Object.values(values)))
+}
