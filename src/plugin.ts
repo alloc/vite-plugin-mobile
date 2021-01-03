9b0917a13b161398ab33daea23812b42a6e55194
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-01-02 21:54:43 -0500

    feat: upgrade to vite@2

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,9 +1,8 @@
-import type { Plugin as RollupPlugin } from 'rollup'
-import { Plugin as VitePlugin, BuildConfig } from 'vite'
+import { rollup, InputOptions, Plugin as RollupPlugin } from 'rollup'
+import { Plugin as VitePlugin } from 'vite'
 import wantsMobile from 'wants-mobile'
+import chalk from 'chalk'
 import path from 'path'
-
-const isBuildMode = process.argv[2] == 'build'
 
 type Config = {
   /**
@@ -31,89 +30,82 @@ export default ({
   const findRoot = (id: string) =>
     Object.values(roots).find(root => id.startsWith(root + '/'))
 
-  if (isBuildMode) {
-    const createRedirectPlugin = (
-      deviceType: 'mobile' | 'desktop',
-      config: Readonly<BuildConfig>
-    ): RollupPlugin => ({
-      name: 'vite-mobile:resolver',
-      async resolveId(id, importer) {
-        // Skip imports from node_modules.
-        if (importer && importer.indexOf(NODE_MODULES_DIR) < 0) {
-          // Resolve relative paths only.
-          if (!/^\.\.?\//.test(id)) return null
-
-          const resolved = await this.resolve(id, importer, {
-            skipSelf: true,
-          })
-          if (resolved) {
-            const moduleId = '/' + path.relative(config.root, resolved.id)
-            const moduleRoot = findRoot(moduleId)
-            if (moduleRoot)
-              return path.join(
-                config.root,
-                moduleId.replace(moduleRoot, roots[deviceType])
-              )
-          }
-        }
-        return null
-      },
-    })
-
-    return {
-      configureBuild(ctx) {
-        type ViteBuild = typeof ctx.builds[number]
-        const mobileBuild: ViteBuild = {
-          input: 'index.html',
-          output: { file: 'index.mobile.html' },
-        }
-
-        // Add the mobile build now to respect plugin order.
-        // It won't start until the main build is finished.
-        ctx.build(mobileBuild)
-        ctx.beforeEach((nextBuild, i) => {
-          if (nextBuild == mobileBuild) {
-            const task = ctx.log.start(`Building mobile bundle...`)
-            mobileBuild.onResult = () => task.done()
-          }
-
-          // The main build is always first.
-          if (i > 0) return
-
-          // Inherit options from the main build.
-          Object.assign(mobileBuild, {
-            ...nextBuild,
-            ...mobileBuild,
-            output: {
-              ...nextBuild.output,
-              ...mobileBuild.output,
-            },
-            plugins: [
-              createRedirectPlugin('mobile', ctx),
-              ...nextBuild.plugins!,
-            ],
-          })
-
-          // The main build needs to redirect mobile imports.
-          nextBuild.plugins!.unshift(createRedirectPlugin('desktop', ctx))
-        })
-      },
-    }
-  }
-
   return {
+    name: 'vite:mobile',
+    enforce: 'post',
+    configResolved(vite) {
+      if (vite.command !== 'build') return
+
+      const createRedirectPlugin = (
+        deviceType: 'mobile' | 'desktop'
+      ): RollupPlugin => ({
+        name: 'vite:mobile:redirect',
+        async resolveId(id, importer) {
+          // Skip imports from node_modules.
+          if (importer && importer.indexOf(NODE_MODULES_DIR) < 0) {
+            // Resolve relative paths only.
+            if (!/^\.\.?\//.test(id)) return null
+
+            const resolved = await this.resolve(id, importer, {
+              skipSelf: true,
+            })
+            if (resolved) {
+              const moduleId = '/' + path.relative(vite.root, resolved.id)
+              const moduleRoot = findRoot(moduleId)
+              if (moduleRoot)
+                return path.join(
+                  vite.root,
+                  moduleId.replace(moduleRoot, roots[deviceType])
+                )
+            }
+          }
+          return null
+        },
+      })
+
+      let inputOptions: InputOptions
+      this.options = (opts: InputOptions) => {
+        // Reuse these options for the mobile build.
+        inputOptions = {
+          ...opts,
+          plugins: [
+            createRedirectPlugin('mobile'),
+            ...opts.plugins!.filter(plugin => plugin.name !== 'vite:mobile'),
+          ],
+        }
+
+        // The main build needs to redirect mobile imports.
+        opts.plugins!.unshift(createRedirectPlugin('desktop'))
+        return null
+      }
+
+      this.generateBundle = async function (outputOptions: any, bundle) {
+        vite.logger.info(chalk.cyan('creating mobile bundle...'))
+        const mobileBundle = await rollup(inputOptions)
+        const { output } = await mobileBundle.generate(outputOptions)
+
+        for (const asset of output) {
+          if (asset.fileName == 'index.html') {
+            asset.fileName = 'index.mobile.html'
+          }
+          if (!bundle[asset.fileName]) {
+            bundle[asset.fileName] = asset
+          }
+        }
+      }
+    },
     configureServer({ app }) {
-      app.use(async (ctx, next) => {
-        if (!ctx.path.startsWith('/@modules/')) {
-          const moduleRoot = findRoot(ctx.path)
+      app.use(async (req, _, next) => {
+        if (req.url && !req.url.startsWith('/@')) {
+          const moduleRoot = findRoot(req.url)
           if (moduleRoot) {
-            const deviceType = wantsMobile(ctx.headers) ? 'mobile' : 'desktop'
+            const deviceType = wantsMobile(req.headers) ? 'mobile' : 'desktop'
             if (moduleRoot !== roots[deviceType]) {
-              ctx.path = ctx.path.replace(moduleRoot, roots[deviceType])
+              req.url = req.url.replace(moduleRoot, roots[deviceType])
             }
           }
         }
-        await next()
+        next()
       })
     },
   }
